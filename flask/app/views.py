494c21b6c335616f79cead44b54b20849ac2ec58
sunsonsu <komson281@gmail.com>
2024-02-11 18:12:14 +0700

    fix signup01

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,9 +5,6 @@ from flask import (jsonify, render_template,
 from sqlalchemy.sql import text
 from app import app
 from app import db
-@app.route('/')
-def homepage():
-    return app.send_static_file("homepage.html")
 
 @app.route('/db')
 def db_connection():
@@ -18,3 +15,12 @@ def db_connection():
         return '<h1>db works.</h1>'
     except Exception as e:
         return '<h1>db is broken.</h1>' + str(e)
+
+@app.route('/')
+def homepage():
+    return app.send_static_file("homepage.html")
+
+@app.route('/signup')
+def signup():
+    return app.send_static_file("signup.html")
+
