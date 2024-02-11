d42338d21aba595b99794f9f42b8fbfdff5c0e0c
sunsonsu <komson281@gmail.com>
2024-02-11 15:34:20 +0700

    merge db branch

--- app/views.py
+++
@@ -1,9 +0,0 @@
-import json
-from flask import (jsonify, render_template,
-                   request, url_for, flash, redirect)
-from app import app
-# from app.forms import forms
-
-@app.route('/')
-def homepage():
-    return app.send_static_file("homepage.html")
